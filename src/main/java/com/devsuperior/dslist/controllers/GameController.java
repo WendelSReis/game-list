4ccc73cfe65314528ac254d19a21d3c90324198e
Wendel S Reis <90618369+WendelSReis@users.noreply.github.com>
2024-01-14 21:56:08 -0300

    Update GameController.java

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -22,12 +22,14 @@ public class GameController {
 	@GetMapping(value = "/{id}")
 	public GameDTO findById(@PathVariable Long id) {
 		GameDTO result = gameService.findById(id);
+		
 		return result;
 	}
 
 	@GetMapping
 	public List<GameMinDTO> findAll() {
 		List<GameMinDTO> result = gameService.findAll();
+		
 		return result;
 	}
 }
